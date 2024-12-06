682297ef373d3ddcd3f974f7c079823170713d58
Jean-Marc <jean-marc.chiappa@ava-ux.com>
2024-12-06 17:49:08 +0100

    add Nucleo L432KC

--- src/utility/dma.c
+++ src/utility/dma.c
@@ -8,8 +8,8 @@ SPI_HandleTypeDef *_hspi1;
 # include "STM32L476/dma.c.opt"
 #elif defined(STM32WB55xx)
 # include "STM32WB55/dma.c.opt"
-// #elif defined(STM32L432xx)
-// # include "l432.init"
+#elif defined(STM32L432xx)
+# include "STM32L432/dma.c.opt"
 #else
 # error "from SPI library : your STM32 model is not supported for now. Please consider to upgrade the library with your STM32 model"
 #endif
